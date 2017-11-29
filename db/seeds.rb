4f716732fcdc0414b7aa97d3a432e822a6716557
Donna <donnamcla@gmail.com>
2017-11-28 21:59:13 -0500

    added some seed data and updated the todo list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-roger = Crew.create(name: 'Roger', rank: 'Lieutenant', specialty: 'dancing', clearance: true, password_digest: BCrypt::Password.create('cilantro'))
-roger.save
 
-voyager = Ship.create(name: 'Voyager', type_class: 'Galaxy', warp_factor: 7, complement: 500, note: "Lost in the delta quadrant")
-voyager.save
+### Crew people ###
+roger = Crew.create(name: 'Roger', rank: 'Sergeant', specialty: 'dancing', clearance: true, password_digest: BCrypt::Password.create('cilantro'))
+roger.save
 
 bobby = Crew.create(name: "Bobby Droptables", rank: 'Ensign', specialty: 'skateboarding', clearance: false, password_digest: BCrypt::Password.create('password'))
 bobby.save
+
+dax = Crew.create(name: "Jadzia Dax", rank: 'Lieutenant', specialty: 'science', clearance: true, password_digest: BCrypt::Password.create('curzon'))
+dax.save
+
+crusher = Crew.create(name: "Dr. Beverly Crusher", rank: 'Commander', specialty: 'medicine', clearance: true, password_digest: BCrypt::Password.create('gates'))
+crusher.save
+
+### Ships ###
+voyager = Ship.create(name: 'Voyager', type_class: 'Galaxy', warp_factor: 7, complement: 500, note: "Lost in the delta quadrant")
+voyager.save
+
+enterprise = Ship.create(name: 'Enterprise', type_class: 'Sovereign', warp_factor: 9.8, complement: 2000, note: "")
+enterprise.save
+
+honshu = Ship.create(name: 'Honshu', type_class: 'Nebula', warp_factor: 6, complement: 200, note: "docked for repair")
+honshu.save
+
+lakota = Ship.create(name: 'Lakota', type_class: 'Excelsior', warp_factor: 8, complement: 1000, note: "test")
+lakota.save
--- spec.md
+++ spec.md
@@ -13,7 +13,7 @@ Specs:
 - [X] ~~*Include login (how e.g. Devise)*~~
 - [X] ~~*Include logout (how e.g. Devise)*~~
 - [ ] Include third party signup/login (how e.g. Devise/OmniAuth)
-- [ ] Include nested resource show or index (URL e.g. users/2/recipes)
+- [X] ~~*Include nested resource show or index (URL e.g. users/2/recipes)*~~
 - [ ] Include nested resource "new" form (URL e.g. recipes/1/ingredients)
 - [ ] Include form display of validation errors (form URL e.g. /recipes/new)
 
@@ -25,4 +25,4 @@ Confirm:
 
 
 ToDo:
-* [ ] prevent duplicate records when adding Ships or Crew
+* [X] ~~*prevent duplicate records when adding Ships or Crew*~~
