91bac4bf60f20ee014ba90063ac7ffabbec157c0
Donna <donnamcla@gmail.com>
2017-12-19 13:08:54 -0500

    changed how keys are accessed, now omniauth works locally again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, 'API_KEY', 'API_SECRET'
+    provider :twitter, ENV['API_KEY'], ENV['API_SECRET']
 end
+
+# use ENV before the keys when retrieving variables from the .env file, omit for the heroku version
