3518212de73825c4890fe3d252fa5a9fb832ebc5
Donna <donnamcla@gmail.com>
2018-07-31 17:43:31 -0400

    finished a basic version of getJson request for showing all ships

--- app/assets/javascripts/ships.js
+++ app/assets/javascripts/ships.js
@@ -10,12 +10,23 @@ class Ship {
 };
 
 function getShips() {
-    $.getJSON("/ships", function(item) {
-        $.forEach(function(item) {
+    let shipUl = document.createElement("ul");
+    shipUl.setAttribute("id", "shipList");
+    let content = document.querySelector(".content");
+    content.appendChild(shipUl);
+
+    const requestUrl = "/ships";
+    const request = new XMLHttpRequest();
+
+    request.open('GET', requestUrl);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        request.response.forEach(function(item) {
             let ship = new Ship(item);
             let shipLi = document.createElement("li");
             shipLi.textContent = ship.name;
-            $("#ships_list").appendChild(shipLi);
+            shipUl.appendChild(shipLi);
         });
-    });
+    };
 };
