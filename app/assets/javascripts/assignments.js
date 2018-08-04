c715f851d39f893f6b2067f66dd42f2fcb80ab7a
Donna <donnamcla@gmail.com>
2018-08-04 13:52:55 -0400

    stubbed out ajax post request

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -7,10 +7,20 @@ function postAssignment(event) {
     if (myForm) {
         myForm.onsubmit = function(event){
             event.preventDefault();
-            alert("I am a hack3r");
-            console.log("I am a hack3r");
+            const xhr = new XMLHttpRequest;
+            const url = '/assignments';
+            const data = JSON.stringify({id: '200'});
+
+            xhr.responseType = 'json';
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    console.log(xhr.response); // code to execute with response
+                }
+            };
+            xhr.open('POST', url);
+            xhr.send(data);
         };
-    }
+    };
 };
 
 
