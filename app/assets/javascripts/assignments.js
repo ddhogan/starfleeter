98b0c1faaa944a8c8aff36d7b303c32b636da1d6
Donna <donnamcla@gmail.com>
2018-08-04 12:39:11 -0400

    added event listener for postAssignment method

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -2,6 +2,16 @@ function getAssignments() {
 
 };
 
-function postAssignment() {
-
+function postAssignment(event) {
+    let myForm = document.querySelector("form")
+    if (myForm) {
+        myForm.onsubmit = function(event){
+            event.preventDefault();
+            alert("I am a hack3r");
+            console.log("I am a hack3r");
+        };
+    }
 };
+
+
+window.onload = postAssignment();
