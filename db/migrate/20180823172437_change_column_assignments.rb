91b9739d3f39ca2d7ffb56f13d8d1b2c3e629925
Donna <donnamcla@gmail.com>
2018-08-23 14:02:24 -0400

    specify foreign keys as integer for pg database, fixed

--- db/migrate/20180823172437_change_column_assignments.rb
+++ db/migrate/20180823172437_change_column_assignments.rb
@@ -1,6 +1,6 @@
 class ChangeColumnAssignments < ActiveRecord::Migration[5.1]
   def change
-    change_column :assignments, :crew_id, :integer
-    change_column :assignments, :ship_id, :integer
+    change_column :assignments, :crew_id, using: 'crew_id::integer'
+    change_column :assignments, :ship_id, using: 'ship_id::integer'
   end
 end
