c141b922234fb83d6e397c3ee87360515afc6ea9
Donna <donnamcla@gmail.com>
2018-11-15 19:29:46 -0500

    update packages for security

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,7 @@ Rails.application.routes.draw do
 
   resources :assignments
 
+  get '/404', :to => 'errors#not_found'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
